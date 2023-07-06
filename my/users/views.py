0c3185cbc1153e6e322da42473a0a90248453225
yeeebin <129045979+yeeebin@users.noreply.github.com>
2023-07-06 15:34:58 +0900

    Add files via upload

---
+++ my/users/views.py
@@ -0,0 +1,28 @@
+from django.contrib.auth.models import User
+from rest_framework import generics, status
+from rest_framework.response import Response
+
+from .serializers import RegisterSerializer, LoginSelializer, ProfileSerializer
+
+from .models import Profile
+
+class RegisterView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = RegisterSerializer
+
+
+class LoginView(generics.GenericAPIView):
+    serializer_class = LoginSelializer
+
+    def post(self, request):
+        serialzer = self.get_serializer(data=request.data)
+        serialzer.is_valid(raise_exception=True)
+        token = serialzer.validated_data
+        return Response({"token":token.key}, status=status.HTTP_200_OK)
+    
+
+class ProfileView(generics.RetrieveUpdateAPIView):
+    queryset = Profile.objects.all()
+    serializer_class = ProfileSerializer
+
+# Create your views here.
