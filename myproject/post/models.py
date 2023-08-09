bfbdbb82bf4964d964c42cad80a8bd7f4b5bfb50
yeeebin <hamy0928@naver.com>
2023-08-10 02:41:43 +0900

    wow

---
+++ myproject/post/models.py
@@ -0,0 +1,39 @@
+from django.db import models
+from django.utils import timezone
+
+# Create your models here.
+class Post(models.model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    body = models.TextField()
+    image = models.ImageField(upload_to='post/', default='default.png')
+    likes = models.ManyToManyField(User)
+    tag = models.ManyToManyField()
+    published_date = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return self.body
+
+
+class Tag(models.model):
+    name = models.CharField(max_length=100)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, default='')
+
+    def __str__(self):
+        return self.name
+    
+
+class Comment(models.Model):
+    comment = models.TextField()
+    date = models.DateTimeField(auto_now_add=True)
+    author = models.ForeignKey(User, on_delete = models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.comment
+
+
+class Reply(models.Model):
+    reply = models.TextField()
+    date = models.DateTimeField(auto_now_add=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, default='')
+    Comment = models.ForeignKey(Comment, on_delete=models.CASCADE)
---
+++ myproject/post/serializers.py
@@ -0,0 +1,23 @@
+from rest_framework import serializers
+from .models import Tag, Post, Comment, Reply
+
+
+class TagSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Tag
+        fields = '__all__'
+
+class PostSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Post
+        fields = '__all__'
+
+class CommentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Comment
+        fields = '__all__'
+
+class ReplySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Reply
+        fields = '__all__'
